17072e442503b8ce9c11495b587218253e6c66fc
Kir <87722197+Machnev999@users.noreply.github.com>
2021-10-05 01:20:22 +0300

    реформ кода

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,10 +3,11 @@ package ru.netology.sqr;
 public class SQRService {
     public int numberSqr(int min, int max) {
         int num = 0;
-        for (int i = 10; i < 99; i++) {
+        for (int i = 10; i <= 99; i++) {
             int square = i * i;
-            if (square >= min && square <= max){
-            num = num + 1;}
+            if (square >= min && square <= max) {
+                num = num + 1;
+            }
         }
         return num;
     }
