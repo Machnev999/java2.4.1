b23d61a8fd5d0582e521a687da256bcd980d770f
Kir <87722197+Machnev999@users.noreply.github.com>
2021-10-05 01:21:26 +0300

    оставил только работающий тест + реформ кода

--- src/test/java/ru.netology.sqr/SQRServiceTest.java
+++ src/test/java/ru.netology.sqr/SQRServiceTest.java
@@ -11,23 +11,8 @@ class SQRServiceTest {
         int min = 200;
         int max = 300;
 
-        int expected = 1;
-        int actual = service.numberSqr(min, max);
-        assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    void shouldNumberSqrIn100To600() {
-        SQRService service = new SQRService();
-        int min = 100;
-        int max = 600;
-
         int expected = 3;
         int actual = service.numberSqr(min, max);
         assertEquals(expected, actual);
-
-
     }
 }
